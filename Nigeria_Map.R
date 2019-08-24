23a8f859996b27f10ad129c243e1422399d150d3
William <izehieghogho@gmail.com>
2019-08-24 06:40:53 -0400

    Add files via upload

---
+++ Nigeria_Map.R
@@ -0,0 +1,39 @@
+
+DATA<-read.csv("C:\\R Stat\\datanigeria.csv",header=T)
+str(DATA)
+(q)
+summary(DATA)
+head(DATA)
+
+# First Second and Third Column with All rows
+DATA[,c(1,1:3)]
+
+map<-leaflet(DATA)%>% addTiles()%>%
+addMarkers(lat=DATA$lat, lng = DATA$long, popup=DATA$name)
+
+library(leaflet)
+m <- leaflet() %>%
+  addTiles(m) %>%  # Add default OpenStreetMap map tiles
+  addMarkers(lat=DATA$lat, lng = DATA$long, popup=DATA$name,
+
+data = getMapData(map))
+
+
+ # Print the map
+m <- leaflet()
+m <- addTiles(m)
+m <- addMarkers(m, lat=DATA$lat, lng = DATA$long, popup=DATA$name)
+m
+
+m %>%
+  # Fitting bounds size and position of the Map frame
+  fitBounds(9.45, 6.95, 4.22, 4.35) %>%
+  addMeasure(
+    position = "bottomleft",
+    primaryLengthUnit = "meters",
+    primaryAreaUnit = "sqmeters",
+    activeColor = "#3D535D",
+    completedColor = "#7D4479")
+
+
+
